63a3e2e18fb4896663b67234612467b4534393f3
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2015-11-07 16:29:57 -0500

    register

--- delivery/views.py
+++ delivery/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout, login, views
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
-    
     return views.login(request)
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return render(request, "registration/register_complete.html")
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration/register.html',{'form':form})
+        
 def user_logout(request):
     logout(request)
