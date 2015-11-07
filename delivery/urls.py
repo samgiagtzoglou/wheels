a166c2649f7b5bb7f38761d8792fceca566b89c2
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2015-11-07 15:29:03 -0500

    login setup

--- delivery/urls.py
+++ delivery/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url('^', include('django.contrib.auth.urls'))
+    
 ]
--- delivery/views.py
+++ delivery/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
 
 def home(request):
     return render(request, 'index.html')
 
-# Create your views here.
+def user_logout(request):
+    logout(request)
